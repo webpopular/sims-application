6d91b51aa7dfb62369f0df83052f9516a95dfb8e
webpopular <bbass@webpopular.net>
2025-10-06 17:54:18 -0500

    add next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        AWS_REGION: process.env.AWS_REGION,
+        MY_AWS_REGION: process.env.MY_AWS_REGION,
+        COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+        COGNITO_USERNAME: process.env.COGNITO_USERNAME,
+        COGNITO_PASSWORD: process.env.COGNITO_PASSWORD,
+        APPSYNC_API_URL: process.env.APPSYNC_API_URL,
+        CLIENT_ID: process.env.CLIENT_ID,
+        NEXT_PUBLIC_CLIENT_ID: process.env.NEXT_PUBLIC_CLIENT_ID,
+    },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
